4a38d4fae2243ae6e6a06925c81c901806f3a937
Ricardo Zamboni <ricardo-testando@hotmail.com>
2023-01-30 01:37:23 -0300

    finish cluster 02

--- chapter-06/ignite-call/src/pages/home/index.tsx
+++ chapter-06/ignite-call/src/pages/home/index.tsx
@@ -1,26 +1,30 @@
+import { ClaimUsernameForm } from '@/components/ClaimUsernameForm'
 import { Heading, Text } from '@ignite-ui/react'
 import Image from 'next/image'
 import PreviewImage from '../../assets/app-preview.png'
 import { Container, Hero, Preview } from './styles'
 
-export default function Home() {
+export function Home() {
   return (
     <Container>
       <Hero>
-        <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text size="lg">
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </Text>
+        <ClaimUsernameForm />
       </Hero>
 
       <Preview>
         <Image
           src={PreviewImage}
           height={400}
-          alt="Calendário simbolizando a aplicação em funcionamento"
-          priority
           quality={100}
+          priority
+          alt="Calendário simbolizando a aplicação em funcionamento"
         />
       </Preview>
     </Container>
